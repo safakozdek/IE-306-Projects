c9ff94e01bd0fbe5f0e3658c8e56ff9eb496a757
Şafak <safakozdek@gmail.com>
2020-05-14 12:15:43 +0300

    Update ks-test.R

--- Homework 2 Due 22 May 2020-20200509/ks-test.R
+++ Homework 2 Due 22 May 2020-20200509/ks-test.R
@@ -4,8 +4,8 @@ data <- read_xls(file_path)
 day1 <- data['Day 1']
 day2 <- data['Day 2']
 unif_dist<-runif(488,0,400)
-ks.test(day1, unif_dist)
-ks.test(day2, unif_dist)
+ks.test(day1, 'punif', 0, 400)
+ks.test(day2, 'punif', 0, 400)
 print('P-value is smaller than 0.05, the claim is rejected.')
 
 
