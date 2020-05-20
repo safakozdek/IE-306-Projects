0ad2595abe04c5d83e0d433a4f25f20fc7950eff
safakozdek <safakozdek@gmail.com>
2020-05-20 12:28:31 +0300

    rename folders

--- Homework 2 Due 22 May 2020-20200509/auto-correlation.R
+++
@@ -1,8 +0,0 @@
-file_path = ''
-data <- read_xls(file_path)
-
-# Auto correlation function
-acf(data$`Day 1`, lag.max = 2, plot = TRUE)
-acf(data$`Day 1`, lag.max = 2, plot = FALSE)
-acf(data$`Day 2`, lag.max = 2, plot = TRUE)
-acf(data$`Day 2`, lag.max = 2, plot = FALSE)
